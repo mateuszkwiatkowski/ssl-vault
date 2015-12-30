cdcef83c1d4bd4aecbf453803e67f4d6c3804b47
Mateusz Kwiatkowski <kwiat@panic.pl>
2015-12-30 23:55:49 +0100

    Bump cookbook version

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license          'The MIT License (MIT)'
 description      'SSL key & certificate storage in chef-vault.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 #version IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue '1.1.1'
-version '1.1.14'
+version '1.1.15'
 
 supports 'ubuntu'
 
